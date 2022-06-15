0b2c67237f4e706861617c4e5873399a31fc2b6c
samuelymh <103726434+samuelymh@users.noreply.github.com>
2022-06-15 17:48:19 +0800

    Test react component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Hello from './Hello'
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hello World! What's up world!</h1>
-    <App />
+    <Hello greeting={'Hello React Ninja'}/>
+    {/* <App /> */}
   </React.StrictMode>
 );
 
