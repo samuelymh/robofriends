9b2c467eed4ea50926b5a7076bf72380149cea8f
samuelymh <103726434+samuelymh@users.noreply.github.com>
2022-07-01 18:26:08 +0800

    feat: Added requestRobots reducers

--- src/reducers.js
+++ src/reducers.js
@@ -1,17 +1,42 @@
-import { CHANGE_SEARCH_FIELD } from "./constants"
+import { 
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_PENDING, 
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED
+} from "./constants";
 
-const initialState = {
+const initialStateSearch = {
   searchField: '',
 }
 
 // This is a reducer - also a pure function
-export const searchRobots = (state=initialState, action={}) => {
+export const searchRobots = (state=initialStateSearch, action={}) => {
   switch(action.type) {
     case CHANGE_SEARCH_FIELD:
       // Two variants (The second version uses spread operator)
       // return Object.assign({}, state, {searchField: action.payload});
       return { ...state, searchField: action.payload }
     default:
+      return state;
+  }
+}
+
+const initialStateRobots = {
+  isPending: false,
+  robots: [],
+  error: ''
+}
+
+export const requestRobots = (state=initialStateRobots, action={}) => {
+  switch(action.type) {
+    case REQUEST_ROBOTS_PENDING:
+      return { ...state, isPending: true };
+    case REQUEST_ROBOTS_SUCCESS:
+      return { ...state, robots: action.payload, isPending: false };
+    case REQUEST_ROBOTS_FAILED:
+      return { ...state, error: action.payload, isPending: false };
+    default:
+      // We want to return state if it doesnt match any of the criteria
       return state;
   }
 }
