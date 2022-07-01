e7423ae93b6a0812a878b13508c00d792816ab07
samuelymh <103726434+samuelymh@users.noreply.github.com>
2022-07-01 14:29:47 +0800

    feat: Installed redux and react-redux

---
+++ src/actions.js
@@ -0,0 +1,7 @@
+// Installation of redux
+// npm install redux
+// npm install react-redux
+
+// components are dumb/presentational components. They don't know that redux exists.
+// redux has:
+// action -> reducer -> store -> make changes
