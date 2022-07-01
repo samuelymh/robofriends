9c79658ccc8e28ad9bef84b240ec7a285c00287f
samuelymh <103726434+samuelymh@users.noreply.github.com>
2022-07-01 16:19:49 +0800

    feat: Implemented redux actions/constants/reducers

--- src/actions.js
+++ src/actions.js
@@ -5,3 +5,17 @@
 // components are dumb/presentational components. They don't know that redux exists.
 // redux has:
 // action -> reducer -> store -> make changes
+
+import { CHANGE_SEARCH_FIELD } from "./constants";
+
+// This is an action
+export const setSearchField = (text) => {
+  console.log(text);
+  return {
+    type: CHANGE_SEARCH_FIELD,
+    payload: text
+  }
+};
+// payload is a naming convention to indicate data to be sent to the reducers
+// type is a constant that we imported from constants file, this is just to avoid
+// typos when typing this type repeatedly.
---
+++ src/reducers.js
@@ -0,0 +1,18 @@
+import { CHANGE_SEARCH_FIELD } from "./constants"
+
+const initialState = {
+  searchField: '',
+}
+
+// This is a reducer - also a pure function
+export const searchRobots = (state=initialState, action={}) => {
+  // console.log(action.type);
+  switch(action.type) {
+    case CHANGE_SEARCH_FIELD:
+      // Two variants (The second version uses spread operator)
+      // return Object.assign({}, state, {searchField: action.payload});
+      return { ...state, searchField: action.payload }
+    default:
+      return state;
+  }
+}
